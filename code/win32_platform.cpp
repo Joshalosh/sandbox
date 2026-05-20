4ad9b29b88bd13e487b04291eb2c0d22eb2b04e3
Joshalosh <josh.jessopsmith@gmail.com>
2026-05-20 23:06:03 +1000

    Begin setting up window draw

--- code/win32_platform.cpp
+++ code/win32_platform.cpp
@@ -1,2 +1,20 @@
 
-// Insert all the code.
+#include <windows.h>
+#include <stdio.h>
+
+int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR command_line, int show_command_line) {
+    WNDCLASS window_class      = {};
+    window_class.style         = CS_HREDRAW|CS_VREDRAW|CS_OWNDC;
+    window_class.lpfWndProc    = Win32MainWindowCallback;
+    window_class.hInstance     = instance;
+    window_class.lpszClassName = "SandboxWindowClass";
+
+    if (RegisterClass(&window_class)) {
+        HWND window = CreateWindowEx(0, window_class.lpszClassName, "Sandbox",
+                                     WS_OVERLAPPEDWINDOW|WS_VISIBLE, CW_USEDEFAULT,
+                                     CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                                     0, 0, instance, 0);
+    }
+
+}
+
